df91eb0f13b349e048d94d8ffe9a52eb2109119e
k0michi <k0michi@koyomi.co>
2025-06-15 22:46:18 +0900

    Set version from git

--- src/main/java/com/koyomiji/robustboats/RobustBoats.java
+++ src/main/java/com/koyomiji/robustboats/RobustBoats.java
@@ -4,6 +4,6 @@ import cpw.mods.fml.common.Mod;
 
 @Mod(modid = RobustBoats.MODID, version = RobustBoats.VERSION)
 public class RobustBoats {
-    public static final String MODID = "robustboats";
-    public static final String VERSION = "0.1.0";
+    public static final String MODID = Tags.MODID;
+    public static final String VERSION = Tags.VERSION;
 }
